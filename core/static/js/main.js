6c936ad7c05e45d7edccbfe65dc3bfc1789814c9
Nycolas Brito do Nascimento <dev.nycolas@outlook.com>
2023-12-10 10:13:03 -0300

    Adding new sections and organizing scheme of page

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -10,3 +10,27 @@ window.addEventListener('scroll', function () {
         navContainer.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.8)';
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var image = document.querySelector('.projetos-image');
+
+    window.addEventListener('scroll', function() {
+        // Altura do contêiner
+        var containerHeight = document.querySelector('.container-projetos').offsetHeight;
+        
+        // Posição atual do rolo
+        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+        // Opacidade máxima
+        var maxOpacity = 1;
+
+        // Calcula a opacidade com base na posição de rolagem
+        var opacity = scrollPosition / containerHeight;
+
+        // Limita a opacidade ao intervalo [0, 1]
+        opacity = Math.max(0, Math.min(opacity, maxOpacity));
+
+        // Aplica a opacidade à imagem
+        image.style.opacity = opacity;
+    });
+});
